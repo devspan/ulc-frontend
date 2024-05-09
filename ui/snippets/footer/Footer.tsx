77320d5d299202de38ee1b99868645f260b1f3d8
nullptr <null@ptr.com>
2024-05-10 02:24:19 +0300

    revert

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import type { CustomLinksGroup } from 'types/footerLinks';
 
 import config from 'configs/app';
+import type { ResourceError } from 'lib/api/resources';
 import useApiQuery from 'lib/api/useApiQuery';
 import useFetch from 'lib/hooks/useFetch';
 import useIssueUrl from 'lib/hooks/useIssueUrl';
@@ -13,13 +14,12 @@ import NetworkAddToWallet from 'ui/shared/NetworkAddToWallet';
 
 import FooterLinkItem from './FooterLinkItem';
 import IntTxsIndexingStatus from './IntTxsIndexingStatus';
+import getApiVersionUrl from './utils/getApiVersionUrl';
 
 const MAX_LINKS_COLUMNS = 4;
 
-const BLOCKSCOUT_LINKS = [
-    {
-    },
-  ];
+const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${ config.UI.footer.frontendVersion }`;
+const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${ config.UI.footer.frontendCommit }`;
 
 const Footer = () => {
 
@@ -28,7 +28,64 @@ const Footer = () => {
       staleTime: Infinity,
     },
   });
+  const apiVersionUrl = getApiVersionUrl(backendVersionData?.backend_version);
   const issueUrl = useIssueUrl(backendVersionData?.backend_version);
+  const BLOCKSCOUT_LINKS = [
+    {
+      icon: 'edit' as const,
+      iconSize: '16px',
+      text: 'Submit an issue',
+      url: issueUrl,
+    },
+    {
+      icon: 'social/canny' as const,
+      iconSize: '20px',
+      text: 'Feature request',
+      url: 'https://blockscout.canny.io/feature-requests',
+    },
+    {
+      icon: 'social/git' as const,
+      iconSize: '18px',
+      text: 'Contribute',
+      url: 'https://github.com/blockscout/blockscout',
+    },
+    {
+      icon: 'social/twitter' as const,
+      iconSize: '18px',
+      text: 'X (ex-Twitter)',
+      url: 'https://www.twitter.com/blockscoutcom',
+    },
+    {
+      icon: 'social/discord' as const,
+      iconSize: '24px',
+      text: 'Discord',
+      url: 'https://discord.gg/blockscout',
+    },
+    {
+      icon: 'discussions' as const,
+      iconSize: '20px',
+      text: 'Discussions',
+      url: 'https://github.com/orgs/blockscout/discussions',
+    },
+    {
+      icon: 'donate' as const,
+      iconSize: '20px',
+      text: 'Donate',
+      url: 'https://github.com/sponsors/blockscout',
+    },
+  ];
+
+  const frontendLink = (() => {
+    if (config.UI.footer.frontendVersion) {
+      return <Link href={ FRONT_VERSION_URL } target="_blank">{ config.UI.footer.frontendVersion }</Link>;
+    }
+
+    if (config.UI.footer.frontendCommit) {
+      return <Link href={ FRONT_COMMIT_URL } target="_blank">{ config.UI.footer.frontendCommit }</Link>;
+    }
+
+    return null;
+  })();
 
   const fetch = useFetch();
 
@@ -63,11 +120,23 @@ const Footer = () => {
       <Box gridArea={ gridArea }>
         <Link fontSize="xs" href="https://scan.ultronsmartchain.io">UltronScan</Link>
         <Text mt={ 3 } fontSize="xs">
-          UltronScan is the Blockchain explorer for Ultron SmartChain.
+          Ultron Smartchain's Blockexplorer.
         </Text>
+        <VStack spacing={ 1 } mt={ 6 } alignItems="start">
+          { apiVersionUrl && (
+            <Text fontSize="xs">
+              Backend: <Link href={ apiVersionUrl } target="_blank">{ backendVersionData?.backend_version }</Link>
+            </Text>
+          ) }
+          { frontendLink && (
+            <Text fontSize="xs">
+              Frontend: { frontendLink }
+            </Text>
+          ) }
+        </VStack>
       </Box>
     );
-  }, []);
+  }, [ apiVersionUrl, backendVersionData?.backend_version, frontendLink ]);
 
   const containerProps: GridProps = {
     as: 'footer',
